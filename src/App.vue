919cd9352b4b86ed914f3a882b9993e45152a942
zhangyun <zhangyun@top-stack.com>
2022-11-29 00:04:53 +0800

    swiper vue setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+      <TopBar/>
+      <SwiperBar/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TopBar from './components/TopBar.vue';
+import SwiperBar from './components/SwiperBar.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopBar,
+    SwiperBar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less" scoped>
 </style>
