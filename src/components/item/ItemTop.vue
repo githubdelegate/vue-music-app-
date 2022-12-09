7bc094feff45061fb1ff3373c4b3e36df633c98a
wz <wangweiappleid@sohu.com>
2022-12-09 11:25:55 +0800

    [fea]: props finish

---
+++ src/components/item/ItemTop.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="itemTop">
+        {{playList}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['playList'],
+    setup(props) {
+       console.log(props.playList);
+    }
+}
+console.log('ItemTop get value');
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/ItemMusic.vue
+++ src/views/ItemMusic.vue
@@ -1,20 +1,32 @@
 <template>
     <div class="itemMusic">
-        歌单页面
+        <ItemTopVue :playList="state.playList"/>
     </div>
 </template>
 
 <script setup>
 import { onMounted } from '@vue/runtime-core';
 import { useRoute } from 'vue-router';
-import {getPlayListDetail} from '@/request/api/itemMusic'
+import ItemTopVue from '@/components/item/ItemTop.vue';
+import { reactive } from 'vue';
+
+// 当引入模块只有一个 export 的时候， 要么使用大括号 要么 加上default ,不然报错找不到
+import { getPlayListDetail } from '@/request/api/itemMusic'
+
+const state = reactive({
+    playList: {}
+})
 
 onMounted(async () => {
      let id = useRoute().query.id;
      console.log(id);
+     state.playList = {
+        "name": "hhhah"
+     }
 
      let res = await getPlayListDetail(id);
      console.log(res);
+     state.playList = res.data.playlist
 })
 
 </script>
