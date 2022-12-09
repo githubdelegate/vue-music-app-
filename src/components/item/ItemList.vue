35c5f2edf08d25a483bde45cf6c71959073e3a02
wz <wangweiappleid@sohu.com>
2022-12-09 17:03:46 +0800

    [feat]: 实现播放列表

--- src/components/item/ItemList.vue
+++ src/components/item/ItemList.vue
@@ -1,5 +1,115 @@
 <template>
-    <div class="itemList">
-        List
+  <div class="itemList">
+    <div class="top">
+      <div class="left">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-bofang"></use>
+        </svg>
+        <span>播放全部</span>
+      </div>
+      <button>+ 收藏({{ subscribedCount }})</button>
     </div>
+    <div class="list">
+      <div class="song" v-for="(song, index) in songs" :key="index">
+      <div class="subleft">
+        <span class="left">{{ index + 1 }}</span>
+        <div class="center">
+          <span class="name">{{ song.name }}</span>
+          <div class="asr">
+            <span class="asrname" v-for="author in song.ar" :key="author">{{
+              author.name
+            }}</span>
+          </div>
+        </div>
+      </div>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-bofang"></use>
+        </svg>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  props: ["songs", "subscribedCount"],
+  setup(props) {
+    console.log("setup props");
+    console.log(props.songs);
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.itemList {
+  display: flex;
+  width: 100%;
+  margin-top: 20px;
+  flex-direction: column;
+  background: white;
+  border-top-left-radius: 0.2rem;
+  border-top-right-radius: 0.2rem;
+  .top {
+    display: flex;
+    height: 0.6rem;
+    justify-content: space-between;
+    margin-left: 0.2rem;
+    margin-right: 0.2rem;
+    margin-top: 0.1rem;
+
+    button {
+      background-color: red;
+      border: 0cm;
+      color: white;
+      border-radius: 0.3rem;
+    }
+  }
+
+  .list {
+    margin-left: 0.2rem;
+    margin-right: 0.2rem;
+    margin-top: 0.2em;
+    .song {
+      display: flex;
+      width: 100%;
+      height: 1rem;
+      align-items: center;
+      justify-content: space-between;
+
+      .subleft {
+        display: flex;
+        align-items: center;
+      }
+      .left {
+        color: lightgray;
+      }
+
+      .center {
+        // max-width: 60%;
+        margin-left: 0.2rem;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        flex-shrink: 5;
+        .name {
+        //   width: 80%;
+          font-weight: bold;
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+
+        .asr {
+          display: flex;
+
+          .asrname {
+            font-weight: normal;
+            color: lightgray;
+            font-size: small;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/ItemMusic.vue
+++ src/views/ItemMusic.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="itemMusic">
         <ItemTopVue :playList="state.playList"/>
+        <ItemListVue :songs="state.songs" :subscribedCount="state.playList.subscribedCount" />
     </div>
 </template>
 
@@ -9,13 +10,14 @@ import { onMounted } from '@vue/runtime-core';
 import { useRoute } from 'vue-router';
 import ItemTopVue from '@/components/item/ItemTop.vue';
 import { reactive } from 'vue';
+import ItemListVue from '@/components/item/ItemList.vue';
 
 // 当引入模块只有一个 export 的时候， 要么使用大括号 要么 加上default ,不然报错找不到
 import { getPlayListDetail, getMusicList } from '@/request/api/itemMusic'
 
 const state = reactive({
     playList: {},
-    musicList: {}
+    songs: {},
 })
 
 onMounted(async () => {
@@ -26,7 +28,8 @@ onMounted(async () => {
      }
 
      let res = await getPlayListDetail(id);
-     console.log('playlist = ' + res);
+     console.log("playlist")
+    console.log(res)
      state.playList = res.data.playlist
      // 缓存数据 sessionStorge
      sessionStorage.setItem('itemDetail', JSON.stringify(res.data.playlist))
@@ -34,8 +37,7 @@ onMounted(async () => {
      let musiclist = await getMusicList(id)
      console.log('musiclist')
      console.log(musiclist)
-
-
+     state.songs = musiclist.data.songs
 })
 
 </script>
